9b97ab99f1f94bce90074cd70e10eadfedb8bbb4
gycsaba <gycsaba@SZT9-24>
2025-02-12 15:18:23 +0100

    SchoolSubject is

--- Kreta.Desktop/ViewModels/SchoolSubjects/SchoolSubjectsViewModel.cs
+++ Kreta.Desktop/ViewModels/SchoolSubjects/SchoolSubjectsViewModel.cs
@@ -8,6 +8,46 @@ namespace Kreta.Desktop.ViewModels.SchoolSubjects
 {
     public partial class SchoolSubjectsViewModel : BaseViewModel
     {
-     
+        private readonly SubjectsManagmentViewModel _subjectsManagmentViewModel;
+        private readonly SubjectsOfSchoollClassesViewModel _subjectsOfSchoollClassesModel;
+        private readonly SubjectsOfTeachersViewModel _subjectsOfTeachersViewModel;
+
+        [ObservableProperty]
+        private BaseViewModel _currentSchoolClassChildView = new SubjectsManagmentViewModel();
+
+        public SchoolSubjectsViewModel()
+        {
+            _subjectsManagmentViewModel = new SubjectsManagmentViewModel();
+            _subjectsOfSchoollClassesModel = new SubjectsOfSchoollClassesViewModel();
+            _subjectsOfTeachersViewModel = new SubjectsOfTeachersViewModel();
+        }
+
+        public SchoolSubjectsViewModel(SubjectsManagmentViewModel subjectsManagmentViewModel, SubjectsOfSchoollClassesViewModel subjectsOfSchoollClassesModel, SubjectsOfTeachersViewModel subjectsOfTeachersViewModel)
+        {
+            _subjectsManagmentViewModel = subjectsManagmentViewModel;
+            _subjectsOfSchoollClassesModel = subjectsOfSchoollClassesModel;
+            _subjectsOfTeachersViewModel = subjectsOfTeachersViewModel;
+            CurrentSchoolClassChildView = _subjectsManagmentViewModel;
+
+        }
+
+        [RelayCommand]
+        private async Task ShowSubjectsManagmentView()
+        {
+            await _subjectsManagmentViewModel.InitializeAsync();
+            CurrentSchoolClassChildView = _subjectsManagmentViewModel;
+        }
+
+        [RelayCommand]
+        private void ShowSubjectsOfSchoollClassesView()
+        {
+            CurrentSchoolClassChildView = _subjectsOfSchoollClassesModel;
+        }
+
+        [RelayCommand]
+        private void ShowSubjectsOfTeachersView()
+        {
+            CurrentSchoolClassChildView = _subjectsOfTeachersViewModel;
+        }
     }
 }
--- Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
+++ Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
@@ -1,10 +1,10 @@
 ﻿using Kreta.Desktop.ViewModels.Base;
+using Kreta.HttpService.Services;
 
 namespace Kreta.Desktop.ViewModels.SchoolSubjects
 {
     public partial class SubjectsManagmentViewModel : BaseViewModel
     {
-       
-
+        private readonly ISubjectHttpService _httpService;
     }
 }
